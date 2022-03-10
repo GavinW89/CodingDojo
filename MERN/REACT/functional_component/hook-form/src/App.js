c1ce341c81518254c205918b29672528c4633274
Gavin Woods <gav5400@gmail.com>
2022-03-10 14:13:07 -0500

    added hook-form assignment

---
+++ MERN/REACT/functional_component/hook-form/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import UserForm from './components/formComponent';
+function App() {
+  return (
+    <div className="App">
+      <UserForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ MERN/REACT/functional_component/hook-form/src/components/formComponent.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+    
+const UserForm = (props) => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");  
+    const [conPassword, setConPassword] = useState("");  
+    
+    return(
+        <div>
+        <form>
+            <div>
+                <label>First Name: </label> 
+                <input type="text" onChange={ (e) => setFirstName(e.target.value) } />
+            </div>
+            <div>
+                <label>Last Name: </label> 
+                <input type="text" onChange={ (e) => setLastName(e.target.value) } />
+            </div>
+            <div>
+                <label>Email Address: </label> 
+                <input type="text" onChange={ (e) => setEmail(e.target.value) } />
+            </div>
+            <div>
+                <label>Password: </label>
+                <input type="text" onChange={ (e) => setPassword(e.target.value) } />
+            </div>
+            <div>
+                <label>Confirm Password: </label>
+                <input type="text" onChange={ (e) => setConPassword(e.target.value) } />
+            </div>
+            <input type="submit" value="Create User" />
+        </form>
+        <p>First Name: {firstName}</p>
+        <p>Last Name: {lastName}</p>
+        <p>Email: {email}</p>
+        <p>Password: {password}</p>
+        <p>Confirm Password: {conPassword}</p>
+
+        </div>
+    );
+};
+    
+export default UserForm;
